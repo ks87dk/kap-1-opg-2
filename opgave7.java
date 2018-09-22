ecb53df425ec27eda1ac2ba60acb9b556e21ac7b
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 19:45:46 +0200

    Add files via upload

---
+++ opgave7.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave7 {
+    public static void main(String[] args) {
+        
+       System.out.println(4 * (1.0 - (1 / 3) + (1 / 5) - (1 / 7)
+                                    + (1 / 9) - (1 / 11)));
+    
+       System.out.println(4 * (1.0 - (1 / 3) + (1 / 5) - (1 / 7)
+                                    + (1 / 9) - (1 / 11) +(1/13)));
+}
+}
