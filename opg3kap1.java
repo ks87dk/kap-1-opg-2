17d92e6b454978db2afc7db491975c9c492507ff
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 19:02:11 +0200

    Add files via upload

---
+++ opg3kap1.java
@@ -0,0 +1,19 @@
+/*
+print a table
+ */
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opg3kap1 {
+    public static void main(String[] args) {
+    System.out.println("   " + "J");
+    System.out.println("J" + "  " + "aaa" + "    " + "v" + "vaaa");
+    System.out.println("J" + "  " + "J" + "  " + "aa" + "   " + "v" + " " + "v"
+            + "v" + "     " + "a" + " " + "a");
+    System.out.println("J   aaaa     v      aaaa");
+        
+}
+}
