f057c4ae8adc416b3ec2bee0dbfda89d6f6d8042
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 16:17:21 +0200

    Add files via upload

---
+++ opgave2kapitel1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave2kapitel1 {
+    public static void main(String[] args) {
+        System.out.println("welcome to Java");
+        System.out.println("welcome to Java");
+        System.out.println("welcome to Java");
+        System.out.println("welcome to Java");
+        System.out.println("welcome to Java");
+        
+    }
+}
