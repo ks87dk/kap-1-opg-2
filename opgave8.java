a478481009601a91c70b0008025cae43a4c08004
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 19:51:30 +0200

    Add files via upload

---
+++ opgave8.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave8 {
+    public static void main(String[] args) {
+		System.out.println("Perimeter = ");
+		System.out.println(2 * 6.5 * 3.14159);
+		System.out.println("Area = ");
+		System.out.println(6.5 * 6.5 * 3.14159);
+    
+}
+}
