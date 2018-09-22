ecead2f0dbb8f27ef9f4e4f5453bca44f9eb82f5
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 19:25:54 +0200

    Add files via upload

---
+++ opgave5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave5 {
+    public static void main(String[] args) {
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+    
+}
