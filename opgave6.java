09e4b1c5320f149c7cbacd8fa1a15ecf1eecb4d8
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-22 19:34:22 +0200

    Add files via upload

---
+++ opgave6.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave6 {
+        public static void main(String[] args) {
+            System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+           
+    }
+
+}
