c1bf44e11967634e789ada48a944c14b6644ca34
ks87dk <42994021+ks87dk@users.noreply.github.com>
2018-09-23 18:57:45 +0200

    Add files via upload

---
+++ opgave10.java
@@ -0,0 +1,19 @@
+
+package helloworld;
+
+/**
+ *
+ * @author ks87d
+ */
+public class opgave10 {
+    
+    public static void main(String[] args) {
+            
+        /* 
+        average speed of a runner, running 15 kilometres in 50 min. and 30 sec.,
+        measured in miles
+        */
+        
+        System.out.println((15 / (50.5 * 1/60)) / 1.6 + (" miles/hour"));
+    }
+}
